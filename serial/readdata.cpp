715bfdb1dfc2876ecb540e9ce304565eb1a3e362
castano.rodrigo <devnull@localhost>
2009-09-07 16:37:31 +0000

    Minor changes.

--- serial/readdata.cpp
+++ serial/readdata.cpp
@@ -1,7 +1,11 @@
 
 #include "petu.h"
 
-inline float deg2rad ( float deg) ;
+// Las pongo aca porque creo que no tiene utilidad en ningun otro lado. Esta bien?
+const float deg2rad_ratio = M_PI/180;
+inline float deg2rad ( float deg) {
+	return deg * deg2rad_ratio;
+}
 
 void readdata(int ndat, std::ifstream &filer, std::vector<float> &cosfi, std::vector<float> &sinfi, std::vector<float> &cossi, std::vector<float> &sinsi) 
 {
@@ -15,14 +19,14 @@ void readdata(int ndat, std::ifstream &filer, std::vector<float> &cosfi, std::ve
 	// Hago este push al principio porque los arrays estan indexados desde el 1 en todo el codigo.
 		
 	while(filer.good()) { 	
-		filer >> fi;
-		filer >> si;
-		
-		
-		cosfi.push_back(cos(deg2rad(fi)));
-		sinfi.push_back(sin(deg2rad(fi)));
-		cossi.push_back(cos(deg2rad(si)));
-		sinsi.push_back(sin(deg2rad(si)));
+		if(filer >> fi && filer >> si) {
+		  
+		  
+		  cosfi.push_back(cos(deg2rad(fi)));
+		  sinfi.push_back(sin(deg2rad(fi)));
+		  cossi.push_back(cos(deg2rad(si)));
+		  sinsi.push_back(sin(deg2rad(si)));
+		  }
 		
 	}
 	// Nota a futuro: se deberia lanzar una Excepcion si el formato del archivo fuera equivocado. 
@@ -30,8 +34,4 @@ void readdata(int ndat, std::ifstream &filer, std::vector<float> &cosfi, std::ve
 }
 
 
-// Las pongo aca porque creo que no tiene utilidad en ningun otro lado. Esta bien?
-const float deg2rad_ratio = M_PI/180;
-inline float deg2rad ( float deg) {
-	return deg * deg2rad_ratio;
-}
+
